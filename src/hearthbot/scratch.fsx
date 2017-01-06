2d2880d991072ef73ca66bad8eaa734e9e46f010
tigerslip <JosephEvensen@hotmail.com>
2017-01-06 18:47:44 -0500

    added hearthbot path

--- src/hearthbot/scratch.fsx
+++ src/hearthbot/scratch.fsx
@@ -1,4 +1,5 @@
 #r "../../packages/suave/lib/net40/suave.dll"
+#r "../../build/hearthbot.exe"
 
 open System
 open Suave                 // always open suave
@@ -6,10 +7,18 @@ open Suave.Successful      // for OK-result
 open Suave.Web             // for config
 open Suave.Operators
 open Suave.Filters
+open Hearthbot.Core.HearthbotCommandParser
+open Hearthbot.Core.HearthstoneApi
 
-let printthis str  = 
-    printfn "%s" str
+let routeParseResult command = 
+    match command with
+        | Some(cmd) -> Query "TVEvw4MKnumshTNOevm3Svrbmkqgp1ukSh5jsn5CDa3g5x6GLM" cmd
+        | None -> "Could not parse hearthbot request"
+
+let run str : string = 
     str
+    |> Parse
+    |> routeParseResult
 
 let getBody req = 
     let getString rawForm = 
@@ -17,6 +26,6 @@ let getBody req =
 
     req.rawForm |> getString
 
-let app : WebPart = POST >=> request (getBody >> printthis >> OK)
+let app : WebPart = POST >=> path "/hearthbot" >=> request (getBody >> run >> OK)
 
 startWebServer defaultConfig app
