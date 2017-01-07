05433f8fb285ed1871e9ee6fe4924da4e5108a85
Joseph Evensen <Joseph Evensen>
2017-01-07 14:11:29 -0500

    added main entry point

--- src/hearthbot/server.fs
+++ src/hearthbot/server.fs
@@ -5,23 +5,33 @@ open Suave.Operators
 open Suave.Filters
 open Hearthbot.Core.HearthbotCommandParser
 open Hearthbot.Core.HearthstoneApi
+open System.Net
+open System
 
-let routeParseResult command = 
-    match command with
-        | Some(cmd) -> Query "TVEvw4MKnumshTNOevm3Svrbmkqgp1ukSh5jsn5CDa3g5x6GLM" cmd
-        | None -> "Could not parse hearthbot request"
+[<EntryPoint>]
+let main [| port |] =
+    let config =
+        { defaultConfig with
+              bindings = [ HttpBinding.mk HTTP IPAddress.Loopback (uint16 port) ]
+              listenTimeout = TimeSpan.FromMilliseconds 3000. }
 
-let run str : string = 
-    str
-    |> Parse
-    |> routeParseResult
+    let routeParseResult command = 
+        match command with
+            | Some(cmd) -> Query "TVEvw4MKnumshTNOevm3Svrbmkqgp1ukSh5jsn5CDa3g5x6GLM" cmd
+            | None -> "Could not parse hearthbot request"
 
-let getBody req = 
-    let getString rawForm = 
-        System.Text.Encoding.UTF8.GetString(rawForm)
+    let run str : string = 
+        str
+        |> Parse
+        |> routeParseResult
 
-    req.rawForm |> getString
+    let getBody req = 
+        let getString rawForm = 
+            System.Text.Encoding.UTF8.GetString(rawForm)
 
-let app : WebPart = POST >=> request (getBody >> run >> OK)
+        req.rawForm |> getString
 
-startWebServer defaultConfig app
+    let app : WebPart = POST >=> request (getBody >> run >> OK)
+
+    startWebServer config app
+    0
