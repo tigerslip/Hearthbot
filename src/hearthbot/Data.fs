2e695ff43337d72612a3f30f526d1e7a5680bf94
Joseph Evensen <Joseph Evensen>
2017-01-08 15:07:37 -0500

    switched to slack complex result

--- src/hearthbot/Data.fs
+++ src/hearthbot/Data.fs
@@ -34,3 +34,25 @@ type Card = {
     text:string
     ``type``:string
 }
+
+type SlackAttachment = {
+    fallback:string // fallback text
+    color:string // e.g. "#36a64f"
+    pretext:string // "Optional text that appears above the attachment block",
+    author_name:string // "Bobby Tables",
+    author_link:string // "http://flickr.com/bobby/",
+    author_icon:string // "http://flickr.com/icons/bobby.jpg",
+    title:string // "Slack API Documentation",
+    title_link:string // "https://api.slack.com/",
+    text:string // "Optional text that appears within the attachment",
+    image_url:string // "http://my-website.com/path/to/image.jpg",
+    thumb_url:string // "http://example.com/path/to/thumb.png",
+    footer:string // "Slack API",
+    footer_icon:string // "https://platform.slack-edge.com/img/default_application_icon.png",
+    ts:int // 123456789
+}
+
+type SlackResponse = {
+    text:string
+    attachments:SlackAttachment array
+}
--- src/hearthbot/SlackFormatter.fs
+++ src/hearthbot/SlackFormatter.fs
@@ -4,16 +4,43 @@ open Data
 open System
 open FSharp.Data
 open FSharp.Collections
+open Newtonsoft.Json
 
-let rec last n xs =
-  if List.length xs <= n then xs
-  else last n xs.Tail
+let serialize<'a> x = 
+    JsonConvert.SerializeObject(x)
 
 let FormatCard card = 
     sprintf "%s\n%s" card.name card.img
 
-let FormatCards cards = 
+let cardsToSlackResponse (cards:Card list) : SlackResponse = 
+    
+    let cardToAttachment card = 
+        {
+            fallback = "";
+            color = "";
+            pretext = "";
+            author_name = "";
+            author_icon = "";
+            author_link = "";
+            title = "";
+            title_link = "";
+            text = "";
+            image_url = card.img;
+            thumb_url = "";
+            footer = "";
+            footer_icon = "";
+            ts = 0
+        }
+    
+    let attachments = 
+        cards 
+        |> Seq.truncate 3
+        |> Seq.map cardToAttachment
+        |> Seq.toArray
 
+    {text = "Results"; attachments = attachments}
+
+let FormatCards cards = 
     cards 
-    |> List.map FormatCard
-    |> String.concat "\n"
+    |> cardsToSlackResponse
+    |> serialize
